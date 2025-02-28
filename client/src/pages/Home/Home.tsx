7c139c780dbcbd888699b826fd8c3aa0fc62560f
Ahmed-Elshhat <169122740+Ahmed-Elshhat@users.noreply.github.com>
2025-02-28 18:20:34 +0200

    add some links in home page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -5,7 +5,12 @@ function Home() {
   return (
     <div>
       <h1>لا إله إلا الله</h1>
-      <Link to="/login">Login</Link>
+      <div className="home-links">
+        <Link to="/login">Login</Link>
+        <Link to="/forgot-password">Forgot Password</Link>
+        <Link to="/verify-reset-code">Verify Reset Code</Link>
+        <Link to="/reset-password">Reset Password</Link>
+      </div>
     </div>
   );
 }
