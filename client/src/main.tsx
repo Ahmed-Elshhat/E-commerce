a05348f97484d90766e59a1bfd50870994fd2140
Ahmed-Elshhat <169122740+Ahmed-Elshhat@users.noreply.github.com>
2025-03-01 20:49:57 +0200

    Add skeleton test

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import "react-loading-skeleton/dist/skeleton.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./Redux/app/store.ts";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
 import "./Home.scss";
-
+import Skeleton from "react-loading-skeleton";
 function Home() {
   return (
-    <div>
+    <div className="Home">
       <h1>لا إله إلا الله</h1>
+      <Skeleton circle width={50} height={50} />
+      <Skeleton width={50} height={50} />
+      <Skeleton width="100%" height={20} />
       <div className="home-links">
         <Link to="/login">Login</Link>
         <Link to="/forgot-password">Forgot Password</Link>
