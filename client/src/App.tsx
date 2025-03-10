b393e2876a2d7ca7c84108ef9b94339a1bffd626
Ahmed-Elshhat <169122740+Ahmed-Elshhat@users.noreply.github.com>
2025-03-10 08:16:31 +0200

    solve signup error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/Auth/NotFound/NotFound";
 import Login from "./pages/Auth/Login/Login";
-import Signup from "./pages/Auth/signup/Signup";
+import Signup from "./pages/Auth/Signup/Signup";
 import Forbidden from "./pages/Auth/Forbidden/Forbidden";
 import RequireAuth from "./pages/Auth/RequireAuth";
 import RequireBack from "./pages/Auth/RequireBack";
